6fd915de1bd0fb22f7936e68e054775537b64f5f
Phurinat Puekkham <peterparkersatom@gmail.com>
2017-10-28 23:40:45 +0700

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 /* global module */
 /* global __dirname */
 var express = require('express');
-
+//This is a comment
 module.exports = function(app) {
   app.use(express.static(__dirname + '/prod'));
 };
